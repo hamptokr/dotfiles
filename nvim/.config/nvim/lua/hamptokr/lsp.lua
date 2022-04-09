9a6a480d020fbd996926bb5d75c891c42b69abd3
Kramer Hampton <me@kramerhampton.dev>
2022-04-09 11:26:44 -0400

    add rust analyzer

--- nvim/.config/nvim/lua/hamptokr/lsp.lua
+++ nvim/.config/nvim/lua/hamptokr/lsp.lua
@@ -113,3 +113,21 @@ require("lspconfig").elixirls.setup(config({
     }
   },
 }))
+
+require("lspconfig").rust_analyzer.setup({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            assist = {
+                importGranularity = "module",
+                importPrefix = "self",
+            },
+            cargo = {
+                loadOutDirsFromCheck = true
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
