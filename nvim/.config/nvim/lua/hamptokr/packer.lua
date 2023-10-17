bd0038750ff229bac2f6b5f693900ad57805c004
Kramer Hampton <me@kramerhampton.dev>
2023-10-16 21:56:05 -0400

    fix(telescope): update version

--- nvim/.config/nvim/lua/hamptokr/packer.lua
+++ nvim/.config/nvim/lua/hamptokr/packer.lua
@@ -9,7 +9,7 @@ return require("packer").startup(function(use)
 
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
+		tag = "0.1.4",
 		-- or                            , branch = '0.1.x',
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
@@ -23,7 +23,6 @@ return require("packer").startup(function(use)
 	use("nvim-treesitter/nvim-treesitter-context")
 	use("theprimeagen/harpoon")
 	use("mbbill/undotree")
-	use("tpope/vim-fugitive")
 	use("mhartington/formatter.nvim")
 
 	use({
