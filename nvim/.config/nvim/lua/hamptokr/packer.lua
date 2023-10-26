a1a722be3ce4cb6bbff70af0bd685c0e6c7bfd91
Kramer Hampton <me@kramerhampton.dev>
2023-10-25 22:42:22 -0400

    fix(packer): duplicates

--- nvim/.config/nvim/lua/hamptokr/packer.lua
+++ nvim/.config/nvim/lua/hamptokr/packer.lua
@@ -18,11 +18,7 @@ return require("packer").startup(function(use)
 	use("theprimeagen/harpoon")
 	use("mbbill/undotree")
 	use("tpope/vim-fugitive")
-
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 	use("nvim-treesitter/nvim-treesitter-context")
-	use("theprimeagen/harpoon")
-	use("mbbill/undotree")
 	use("mhartington/formatter.nvim")
 
 	use({
